48416eca523e59def7620609300b33422d64a2b6
Feddy <2523474954@qq.com>
2023-04-18 20:35:14 +0800

    i

---
+++ src/base/CoffeeBrewer.java
@@ -0,0 +1,27 @@
+package base;
+
+public class CoffeeBrewer extends CoffeeAccessory{
+    static String model = "";
+    static String waterSupply = "";
+    static int capacity =0;
+    //get
+    public static String getModel(){
+        return model;
+    }
+    public static String getWaterSupply(){
+        return waterSupply;
+    }
+    public static int getCapacity(){
+        return capacity;
+    }
+    //set
+    public static void setModel(String newModel){
+        model=newModel;
+    }
+    public static void setWaterSupply(String newWaterSupply){
+        waterSupply = newWaterSupply;
+    }
+    public static void setCapacity(int newCapacity){
+        capacity= newCapacity;
+    }
+}
